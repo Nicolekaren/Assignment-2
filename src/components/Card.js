b77a4ad3b5a7e6fa08e2c6b6095f397e24b8417e
Nicolekaren <nicole.chiguil68@myhunter.cuny.edu>
2022-11-16 00:51:11 -0500

    Added a function to handle when the user clicks on the two cards and used a useEffect hook to display the cards when initially loading the game

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,17 @@
 import './Card.css'
 
-function Card({card}) {
+function Card({card, handleChoice}) {
+    const handleClick = () => {
+        handleChoice(card)
+    }
 
     return (
         <div className="card">
-            <div>
-                <img className ="front" src = {card.src} alt="image of front of card"/>
-                <div 
-                    style={{height:'100px', width:'100px', backgroundColor:'#4E4459', borderStyle:'solid', borderColor:'white'}} >
-                </div>
-              </div>
+                <img className="front" src = {card.src} alt="image of front"/>
+                <img className="back"  onClick={handleClick} />
+
         </div>
+          
     
     );
 }
